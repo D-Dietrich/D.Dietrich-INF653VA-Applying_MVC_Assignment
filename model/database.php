71c5331fc9455fdaa7825f03af04fd29858818a9
D-Dietrich <89669360+D-Dietrich@users.noreply.github.com>
2022-03-12 10:02:30 -0600

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,10 +1,11 @@
 <?php
-$dsn = 'mysql:host=localhost; dbname=todolist';
-$username = 'root';
+$dsn = 'mysql:host=m7az7525jg6ygibs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=mvagsta26o5b3jpu';
+$username = 'jcfk9fsv3m030zuq';
+$p = 'glxgh2xo370kspw2';
 
 
 try {
-    $db =new PDO($dsn, $username);
+    $db =new PDO($dsn, $username, $p);
 
 
 } catch (PDOException $e) {
